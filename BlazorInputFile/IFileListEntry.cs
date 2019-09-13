69c82bab3dc92b33bd4fd3b13e38d73972906a77
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-09-13 09:28:48 +0100

    Initial state

---
+++ BlazorInputFile/IFileListEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace BlazorInputFile
+{
+    public interface IFileListEntry
+    {
+        DateTime LastModified { get; }
+
+        string Name { get; }
+
+        long Size { get; }
+
+        string Type { get; }
+
+        Stream Data { get; }
+
+        event EventHandler OnDataRead;
+    }
+}
---
+++ samples/Sample.WebAssembly/Startup.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Sample.Core;
+
+namespace Sample.WebAssembly
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
